83f54e7d35017da8f6e1c234a98f72f7affe0405
alex-galati <alexanderjohn2005@gmail.com>
2025-11-24 01:00:27 +0000

    Implementing the code for detach

--- test.c
+++ test.c
@@ -5,15 +5,18 @@
 
 int main(int argc, char* argv[]) 
 {
+	const char *filename = "testFS.bvfs";
+	remove(filename);
+
     	printf("=== BVFS TEST START ===\n");
 		
 	printf("[TEST] Opening a file from a fs that doesnt exist...\n");
 	if(bvfs_open("testfile.txt", BVFS_WRCONCAT) > -1)
 	{
-		printf("ERROR: Error somewhere. Should be returning -1.");
+		printf("ERROR: Error somewhere. Should be returning -1.\n");
 		return 1;
 	}
-	printf("[OK] testfile.txt not opened.");
+	printf("[OK] testfile.txt not opened.\n\n");
     	
     	printf("[TEST] Attaching filesystem...\n");
     	if (bvfs_attach((char*)"testFS.bvfs") != 0) 
@@ -21,22 +24,39 @@ int main(int argc, char* argv[])
 		printf("ERROR: Could not attach FS.\n");
         	return 1;
     	}
-    	printf("[OK] FS attached.\n");
+    	printf("[OK] FS attached.\n\n");
 
 	printf("[TEST] Opening a file that doesnt exist in read only...\n");
 	if(bvfs_open("testfile.txt", BVFS_RDONLY) > -1)
 	{
-		printf("ERROR: Error somewhere. Should be returning -1.");
+		printf("ERROR: Error somewhere. Should be returning -1.\n");
 		return 1;
 	}
-    	printf("[OK] testfile.txt not opened.\n");
-	
+    	printf("[OK] testfile.txt not opened.\n\n");
+		
 	printf("[TEST] Opening a file in WRTRUNC so it actually is created...\n");
 	if(bvfs_open("testfile.txt", BVFS_WRTRUNC) < 0)
 	{
-		printf("ERROR: Error somewhere. Probably because you cant read from a file that doesnt exist.");
+		printf("ERROR: Error somewhere \n");
 		return 1;
 	}
-	printf("[OK] testfile.txt opened.");
+	printf("[OK] testfile.txt opened \n\n");
+	
+	printf("[TEST] Attempting to detach the file system...\n");
+	if(bvfs_detach() < 0)
+	{
+		printf("ERROR: Error somewhere. \n");
+		return 1;
+	}
+	printf("[OK] testfile.txt detached.\n\n");
+	
+	printf("[TEST] Attempting to detach the file system again...\n");
+	if(bvfs_detach() != -1)
+	{
+		printf("ERROR: Error somewhere. Somehow tried to detach anyway.\n");
+		return 1;
+	}
+	printf("[OK] testfile.txt still detached.\n\n");
+
 }
 
